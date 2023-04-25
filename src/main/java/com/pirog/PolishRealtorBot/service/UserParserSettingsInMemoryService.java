e372e9073fe98772d9789d96966731ab0ed92458
Vitalik <vitalikvlasenko88@gmail.com>
2023-04-25 21:42:50 +0300

    add in memory service

--- src/main/java/com/pirog/PolishRealtorBot/service/UserParserSettingsInMemoryService.java
+++ src/main/java/com/pirog/PolishRealtorBot/service/UserParserSettingsInMemoryService.java
@@ -1,7 +1,29 @@
 package com.pirog.PolishRealtorBot.service;
 
+import com.pirog.PolishRealtorBot.dao.entity.UserParserSettings;
+import com.pirog.PolishRealtorBot.dao.repository.UserParserSettingsInMemoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserParserSettingsInMemoryService {
+
+    @Autowired
+    private UserParserSettingsInMemoryRepository memoryRepository;
+
+    public void save(long userId, UserParserSettings userSettings) {
+        memoryRepository.save(userId, userSettings);
+    }
+
+    public UserParserSettings get(long userId) {
+        return memoryRepository.find(userId);
+    }
+
+    public void update(long userId, UserParserSettings userParserSettings) {
+        memoryRepository.update(userId, userParserSettings);
+    }
+
+    public void delete(long userId) {
+        memoryRepository.delete(userId);
+    }
 }
