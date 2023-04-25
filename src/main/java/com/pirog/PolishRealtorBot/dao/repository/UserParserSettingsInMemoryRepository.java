c7776ec2ab0bbb34b9b09a251932a96bc62214e6
Vitalik <vitalikvlasenko88@gmail.com>
2023-04-25 21:38:46 +0300

    add in memory repository add custom exception

--- src/main/java/com/pirog/PolishRealtorBot/dao/repository/UserParserSettingsInMemoryRepository.java
+++ src/main/java/com/pirog/PolishRealtorBot/dao/repository/UserParserSettingsInMemoryRepository.java
@@ -1,8 +1,33 @@
 package com.pirog.PolishRealtorBot.dao.repository;
 
+import com.pirog.PolishRealtorBot.dao.entity.UserParserSettings;
+import com.pirog.PolishRealtorBot.exception.entity.NoSuchUserException;
 import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Repository
 public class UserParserSettingsInMemoryRepository {
+    private final Map<Long, UserParserSettings> database = new HashMap<>();
 
+    public void save(long userId, UserParserSettings userSettings) {
+        database.put(userId, userSettings);
+    }
+
+    public UserParserSettings find(long userId) {
+        return database.get(userId);
+    }
+
+    public void update(long userId, UserParserSettings userParserSettings) {
+        UserParserSettings existSettings = find(userId);
+        if (existSettings == null) {
+            throw new NoSuchUserException(userId);
+        }
+        save(userId, userParserSettings);
+    }
+
+    public void delete(long userId) {
+        database.remove(userId);
+    }
 }
---
+++ src/main/java/com/pirog/PolishRealtorBot/service/UserParserSettingsInMemoryService.java
@@ -0,0 +1,7 @@
+package com.pirog.PolishRealtorBot.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserParserSettingsInMemoryService {
+}
